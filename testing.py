e24ab6bde5a74e26e4c488cd73cbf1b7a22cec20
christinevandev <54376653+christinevandev@users.noreply.github.com>
2021-05-27 10:21:15 -0700

    Add files via upload

---
+++ testing.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# ## voila app
+
+# In[1]:
+
+
+import pandas as pd
+
+
+# In[2]:
+
+
+import matplotlib.pyplot as plt
+
+
+# In[3]:
+
+
+plt.plot([1, 2, 3, 4])
+plt.ylabel('some numbers')
+plt.show()
+
+
+# In[1]:
+
+
+get_ipython().system(' jupyter nbextension enable --py widgetsnbextension --sys-prefix')
+
+
+# In[2]:
+
+
+get_ipython().system(' jupyter serverextension enable voila --sys-prefix')
+
+
+# In[ ]:
+
+
+
+
